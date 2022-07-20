f6a622fe34ba7524d60e56b088f05eabfd52093c
Matteo Castellani <github@t-hoster.com>
2022-07-20 14:28:24 +0100

    Update main.tf

--- Magento/one-webserver-one-db/main.tf
+++ Magento/one-webserver-one-db/main.tf
@@ -123,7 +123,7 @@ resource "ionoscloud_server" "database01" {
   ram               = 4096
   cpu_family        = "INTEL_SKYLAKE"
   availability_zone = "ZONE_1"
-  image_name        = "45ec8032-e309-11eb-a927-824af8c35c96"
+  image_name        = "a8638113-0768-11ed-b441-96ff36bbb3ad"
   ssh_key_path      = [ "${var.ssh_pub_key}" ]
   #  image_password    = var.console_password
   volume {
